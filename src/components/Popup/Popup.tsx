6e9061bc727473fec45254b0f851ef838fd40d77
charlieweinberger <charlieweinberger05@gmail.com>
2025-05-15 21:07:55 -0700

    fixed bug in officer popup, and separated popup function into several helper functions

--- src/components/Popup/Popup.tsx
+++ src/components/Popup/Popup.tsx
@@ -4,13 +4,98 @@ import Link from "next/link";
 import { X } from "lucide-react";
 import "./Popup.css";
 
-interface PopupProps {
+function PopupItem({ name, content }: { name: string; content: string }) {
+  return (
+    <div>
+      <h3>{name}</h3>
+      <p>{content}</p>
+    </div>
+  );
+}
+
+function EventPopup({
+  event,
+  closePopup,
+}: {
+  event: AACEvent;
+  closePopup: () => void;
+}) {
+  return (
+    <>
+      <div className="popup-header">
+        <h3>{event.name}</h3>
+        <div className="popup-buttons">
+          <X className="popup-close-icon" onClick={() => closePopup()} />
+        </div>
+      </div>
+      <div className="popup-body">
+        <PopupItem name="Date" content={event.date} />
+        <PopupItem name="Event Info" content={event.description} />
+        {/* For future events show sign-up link */}
+        {event.signUpLink && (
+          <Link
+            href={event.signUpLink}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="signup-link"
+          >
+            <h3 className="signup-link-text">Click here to sign up!</h3>
+          </Link>
+        )}
+      </div>
+    </>
+  );
+}
+
+function OfficerPopup({
+  officer,
+  closePopup,
+}: {
+  officer: Officer;
+  closePopup: () => void;
+}) {
+  return (
+    <>
+      <div className="popup-header">
+        <h3 className={`popup-title-${officer.id}`}>{officer.name}</h3>
+        <div className="popup-buttons">
+          <Link href={officer.instagram} target="_blank" rel="noopener">
+            <Image
+              src="/logos/instagram.svg"
+              alt="Instagram Logo"
+              width={20}
+              height={0}
+              className="popup-instagram-icon"
+            />
+          </Link>
+          <X className="popup-close-icon" onClick={() => closePopup()} />
+        </div>
+      </div>
+      <div className="popup-body">
+        <PopupItem name="Role" content={officer.role} />
+        <PopupItem name="Major" content={officer.major} />
+        <PopupItem
+          name={`Why I ${officer.id === "aristani" ? "created" : "joined"} AAC`}
+          content={officer.whyJoined}
+        />
+        <PopupItem
+          name="My favorite AAC Memory..."
+          content={officer.favoriteMemory}
+        />
+      </div>
+    </>
+  );
+}
+
+export default function Popup({
+  datum,
+  dataType,
+  closePopup,
+}: {
   datum: PolaroidType;
   dataType: PolaroidTypeName;
   closePopup: () => void;
-}
-
-export default function Popup({ datum, dataType, closePopup }: PopupProps) {
+}) {
   const ref = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
@@ -23,77 +108,14 @@ export default function Popup({ datum, dataType, closePopup }: PopupProps) {
     return () => document.removeEventListener("mousedown", handleClickOutside);
   }, [closePopup]);
 
-  if (dataType === "event") {
-    const event = datum as AACEvent;
-    return (
-      <div className="popup">
-        <div className="popup-content" ref={ref}>
-          <div className="popup-header">
-            <h3>{event.name}</h3>
-            <div className="popup-buttons">
-              <X className="popup-close-icon" onClick={() => closePopup()} />
-            </div>
-          </div>
-          <div className="popup-body">
-            <div>
-              <h3>Date</h3>
-              <p>{event.date}</p>
-            </div>
-            <div>
-              <h3>Event Info</h3>
-              <p>{event.description}</p>
-            </div>
-            {/* For future events show sign-up link */}
-            {event.signUpLink && (
-              <Link
-                href={event.signUpLink}
-                target="_blank"
-                rel="noopener noreferrer"
-                className="signup-link"
-              >
-                <h3 className="signup-link-text">
-                  Click here to sign up!
-                </h3>
-              </Link>
-            )}
-          </div>
-        </div>
-      </div>
-    );
-  }
-
-  const officer = datum as Officer;
-
   return (
     <div className="popup">
-      <div className={`popup-content popup-background-${officer.id}`} ref={ref}>
-        <div className="popup-header">
-          <h3 className={`popup-title-${officer.id}`}>{officer.name}</h3>
-          <div className="popup-buttons">
-            <Link href={officer.instagram} target="_blank" rel="noopener">
-              <Image
-                src="/logos/instagram.svg"
-                alt="Instagram Logo"
-                width={20}
-                height={0}
-                className="popup-instagram-icon"
-              />
-            </Link>
-            <X className="popup-close-icon" onClick={() => closePopup()} />
-          </div>
-        </div>
-        <div>
-          <h3 className="popup-prompt">Role</h3>
-          <p>{officer.role}</p>
-          <h3 className="popup-prompt">Major</h3>
-          <p>{officer.major}</p>
-          <h3 className="popup-prompt">
-            Why I {officer.id === "aristani" ? "created" : "joined"} AAC
-          </h3>
-          <p>{officer.whyJoined}</p>
-          <h3 className="popup-prompt">My favorite AAC Memory...</h3>
-          <p>{officer.favoriteMemory}</p>
-        </div>
+      <div className={`popup-content popup-background-${datum.id}`} ref={ref}>
+        {dataType === "event" ? (
+          <EventPopup event={datum as AACEvent} closePopup={closePopup} />
+        ) : (
+          <OfficerPopup officer={datum as Officer} closePopup={closePopup} />
+        )}
       </div>
     </div>
   );
