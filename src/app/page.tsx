03b7dc359a35ab372a7bc941835f0af644507b48
charlieweinberger <charlieweinberger05@gmail.com>
2026-01-17 18:57:01 -0800

    added background image to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,10 +6,12 @@ import "./page.css";
 export default function Home() {
   return (
     <div className="home">
-      <h1>Anteater Adventure Club</h1>
-      <Link href="/about" className="button cta-button">
-        Join the Adventure!
-      </Link>
+      <div className="home-content">
+        <h1>Anteater Adventure Club</h1>
+        <Link href="/about" className="button cta-button">
+          Join the Adventure!
+        </Link>
+      </div>
     </div>
   );
 }
