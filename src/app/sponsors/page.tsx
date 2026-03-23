2a7fdf63b1d55e87d674f873b771bcd9cd5c5529
charlieweinberger <charlieweinberger05@gmail.com>
2026-03-23 02:43:08 -0700

    added initial sponsors page

---
+++ src/app/sponsors/page.tsx
@@ -0,0 +1,14 @@
+export default function Sponsors() {
+  return (
+    <div className="sponsors-page">
+      <div className="text-center">
+        <h1>Our Sponsors</h1>
+      </div>
+      <p>
+        We are grateful for the generous support of our sponsors, who help make
+        our events and retreats possible. If you are interested in becoming a
+        sponsor, please contact us at anteateradventureclub@gmail.com.
+      </p>
+    </div>
+  );
+}
